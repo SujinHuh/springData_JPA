35f9668935987be1d397471e5f20d2588b91500e
SujinHuh <juin483@naver.com>
2020-01-13 23:10:31 +0900

    12. JPA 프로그래밍: Query - 2
    Criteria
      ● 타입 세이프 쿼리 : 문자열이 들어가지 않는다.
    
    * JPOL : 단점은 타입세이프하니 않는것 오타가 발생할 수 있음

--- src/main/java/me/whiteship/demospringdata/JpaRunner.java
+++ src/main/java/me/whiteship/demospringdata/JpaRunner.java
@@ -9,6 +9,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -23,11 +26,13 @@ public class JpaRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+        query.select(root);
 
-        List<Post> posts = query.getResultList();
+        List<Post> posts = entityManager.createQuery(query).getResultList();
         posts.forEach(System.out::println);
-
     }
 
 }
