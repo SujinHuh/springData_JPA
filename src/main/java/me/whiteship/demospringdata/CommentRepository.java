a41b86958860bf73244728e17b1197aaa38983ec
SujinHuh <juin483@naver.com>
2020-01-15 23:20:43 +0900

    17. 스프링 데이터 Common: Repository 인터페이스 정의하기 - 1
    Repository 인터페이스로 공개할 메소드를 직접 일일히 정의하고 싶다면 특정 리포지토리 당
     ● @RepositoryDefinition
    
    @RepositoryDefinition(domainClass = Comment.class, idClass = Long.class) public interface CommentRepository {
        Comment save(Comment comment);
        List<Comment> findAll();
    }
    
    공통 인터페이스 정의
      ● @NoRepositoryBean
    
    @NoRepositoryBean
    public interface MyRepository<T, ID extends Serializable> extends Repository<T, ID> {
        <E extends T> E save(E entity);
        List<T> findAll();
    }

---
+++ src/main/java/me/whiteship/demospringdata/CommentRepository.java
@@ -0,0 +1,6 @@
+package me.whiteship.demospringdata;
+
+
+public interface CommentRepository extends MyRepository<Comment,Long>{
+
+}
---
+++ src/main/java/me/whiteship/demospringdata/MyRepository.java
@@ -0,0 +1,18 @@
+package me.whiteship.demospringdata;
+
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@NoRepositoryBean
+public interface MyRepository<T,id extends Serializable> extends Repository<T,id> {
+
+    <E extends T> E save(E entity);
+
+    List<T> findAll();
+
+    long count();
+
+}
