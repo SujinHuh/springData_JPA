7c4f67737b981070e6847ecc9fbf241ae6e3c1f6
SujinHuh <juin483@naver.com>
2020-01-15 15:59:35 +0900

    16. 스프링 데이터 Common: Repository - 2
     * Hibernate:
          select
              post0_.id as id1_2_,
              post0_.title as title2_2_
          from
              post post0_
          where
              post0_.title like ? escape ? limit ?
    
       * binding parameter [1] as [VARCHAR] - [%Hi%]

--- src/main/java/me/whiteship/demospringdata/PostRpository.java
+++ src/main/java/me/whiteship/demospringdata/PostRpository.java
@@ -1,10 +1,11 @@
 package me.whiteship.demospringdata;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRpository extends JpaRepository<Post,Long> {
 
-
-
+    Page<Post> findByTitleContains(String titile, Pageable pageable);
 }
 
--- src/test/java/me/whiteship/demospringdata/PostRpositoryTest.java
+++ src/test/java/me/whiteship/demospringdata/PostRpositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -39,8 +41,27 @@ public class PostRpositoryTest {
 
         //When
         List<Post> posts = postRpository.findAll();
+
+        //Then
         assertThat(posts.size()).isEqualTo(1);
         assertThat(posts.contains(newPost));
+
+        //When
+        Page<Post> page = postRpository.findAll(PageRequest.of(0, 10));
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        //When
+        page = postRpository.findByTitleContains("Hi",PageRequest.of(0,10));
+
+        //ThenR
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
     }
 
 }
