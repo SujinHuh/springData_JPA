56cedabd1007c9a372d875eadf60aa23f015b024
SujinHuh <juin483@naver.com>
2020-01-15 11:27:52 +0900

    14. 핵심 개념 이해 정리 - 1
    데이터베이스와 자바
    
    패러다임 불일치
    
    ORM이란?
    
    JPA 사용법 (엔티티, 벨류 타입, 관계 맵핑)
    
    JPA 특징 (엔티티 상태 변화, Cascade, Fetch, 1차 캐시, ...) 주의할 점
      ● 반드시 발생하는 SQL을 확인할 것.
      ● 팁: “?”에 들어있는 값 출력하기
        ○ logging.level.org.hibernate.SQL=debug
        ○ logging.level.org.hibernate.type.descriptor.sql=trace
    
        * springRepository를 사용해서 들어가는 '실제'값을 확인해볼 수 있다.
    
        binding parameter [1] as [VARCHAR] - [Spring]
        binding parameter [2] as [BIGINT] - [5]

--- src/main/java/me/whiteship/demospringdata/JpaRunner.java
+++ src/main/java/me/whiteship/demospringdata/JpaRunner.java
@@ -1,34 +1,29 @@
 package me.whiteship.demospringdata;
 
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
-import java.util.List;
+
 
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
     @Autowired
-   Sujin sujin;
+    PostRpository postRpository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        System.out.println("===============");
-        System.out.println(sujin.getName());
+        Post post =new Post();
+        post.setTitle("Spring");
 
+        Comment comment = new Comment();
+        comment.setComment("hello");
+
+        postRpository.save(post);
     }
 
 }
