d37d93706b0fba96a850383d80266e1904d9fa75
SujinHuh <juin483@naver.com>
2020-01-16 23:22:47 +0900

    19. 스프링 데이터 Common: 쿼리 만들기 개요 - 1
    스프링 데이터 저장소의 메소드 이름으로 쿼리 만드는 방법
    
      ● 메소드 이름을 분석해서 ​쿼리 만들기​ (CREATE)
      ● 미리 정의해 둔 ​쿼리 찾아 사용하기​ (USE_DECLARED_QUERY)
      ● 미리 정의한 쿼리 찾아보고 없으면 만들기 (CREATE_IF_NOT_FOUND)
    
      * 메소드 이름을 분석해서 ​쿼리 만들기​ (CREATE)

--- src/main/java/me/whiteship/demospringdata/CommentRepository.java
+++ src/main/java/me/whiteship/demospringdata/CommentRepository.java
@@ -1,6 +1,9 @@
 package me.whiteship.demospringdata;
 
+import java.util.List;
 
 public interface CommentRepository extends MyRepository<Comment,Long>{
+
+    List<Comment> findByTitleContains(String keyword);
 
 }
