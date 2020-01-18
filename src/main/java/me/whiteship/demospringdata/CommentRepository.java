ba241102d5ff4d113e44a4b3b4f3bb63d49b68aa
SujinHuh <juin483@naver.com>
2020-01-18 14:56:46 +0900

    20. 스프링 데이터 Common: 쿼리 만들기 실습 - 2
    * CommentRepository 쿼리 만들기
    
       - findByCommentContainsIgnoreCaseAndLikeCountGreaterThan

--- src/main/java/me/whiteship/demospringdata/CommentRepository.java
+++ src/main/java/me/whiteship/demospringdata/CommentRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 public interface CommentRepository extends MyRepository<Comment,Long>{
 
-    List<Comment> findByCommentContainsIgnoreCase(String keyword);
+    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword,Integer count);
 }
