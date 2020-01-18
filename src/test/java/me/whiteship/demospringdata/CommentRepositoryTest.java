38dab9eeb193ee883b183925a7c35ca8f23d607f
SujinHuh <juin483@naver.com>
2020-01-18 16:28:44 +0900

    20. 스프링 데이터 Common: 쿼리 만들기 실습 - 2
    * CommentRepository 쿼리 만들기
    
      - Page​<User> findByLastname(String lastname, pageable);

--- src/test/java/me/whiteship/demospringdata/CommentRepositoryTest.java
+++ src/test/java/me/whiteship/demospringdata/CommentRepositoryTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,10 +27,11 @@ public class CommentRepositoryTest {
         this.createComment(100,"spring data jpa");
         this.createComment(55,"HIBERNATE SPRING");
 
+        PageRequest pageRequest = PageRequest.of (0,10, Sort.by(Sort.Direction.DESC,"LikeCount"));
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountAsc("Spring");
-        assertThat(comments.size()).isEqualTo(2);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("LikeCount",55);
+        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("Spring",pageRequest);
+        assertThat(comments.getTotalElements()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("LikeCount",100);
     }
 
     private void createComment(int likeCount, String comment) {
